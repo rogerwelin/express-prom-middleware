71984d953c811a6778906695a2691f6379f54142
Roger Welin <roger.welin@icloud.com>
2023-02-17 11:04:29 +0100

    implement metrics for http errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import { collectDefaultMetrics, register } from 'prom-client';
-import { inProgress, totalRequests, responseLatency } from './metrics';
+import { ongoingRequests, requestCount, responseDuration, errorCount, clientErrorCount } from './metrics';
 
 export interface PromOptions {
   metricsPath?: string;
@@ -20,17 +20,23 @@ export const promMiddleware = (options: PromOptions) => {
 
     if (req.path !== options.metricsPath) {
       start = process.hrtime.bigint();
-      inProgress.inc({ path: req.path, method: req.method });
+      ongoingRequests.inc({ path: req.path, method: req.method });
     }
 
     res.on('finish', () => {
       if (req.originalUrl !== options.metricsPath) {
-        inProgress.dec({ path: req.originalUrl, method: req.method });
-        totalRequests.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
+        ongoingRequests.dec({ path: req.originalUrl, method: req.method });
+        requestCount.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
         const end = process.hrtime.bigint();
         // Calculate the difference between the start time and end time in milliseconds
         const diffInMs = Number(end - start) / 1000000;
-        responseLatency.observe({ path: req.originalUrl, method: req.method }, diffInMs);
+        responseDuration.observe({ path: req.originalUrl, method: req.method }, diffInMs);
+        if (res.statusCode >= 500 && res.statusCode <= 511) {
+          errorCount.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
+        }
+        if (res.statusCode >= 400 && res.statusCode <= 451) {
+          clientErrorCount.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
+        }
       }
     });
 
--- src/metrics.ts
+++ src/metrics.ts
@@ -1,19 +1,31 @@
 import { Counter, Gauge, Histogram, Summary } from 'prom-client';
 
-export const inProgress = new Gauge({
+export const ongoingRequests = new Gauge({
   name: 'http_requests_in_progress',
   help: 'the count of HTTP requests that are currently in process',
   labelNames: ['path', 'method'],
 });
 
-export const totalRequests = new Counter({
+export const requestCount = new Counter({
   name: 'http_requests_total',
   help: 'the total number of HTTP requests',
   labelNames: ['path', 'method', 'statuscode'],
 });
 
-export const responseLatency = new Summary({
+export const responseDuration = new Summary({
   name: 'http_response_latency_ms',
   help: 'the duration of responses in milliseconds',
   labelNames: ['path', 'method'],
+});
+
+export const errorCount = new Counter({
+  name: 'http_errors_total',
+  help: 'the overall number of HTTP errors',
+  labelNames: ['path', 'method', 'statuscode'],
+});
+
+export const clientErrorCount = new Counter({
+  name: 'http_errors_client_total',
+  help: 'the total number of client-side errors',
+  labelNames: ['path', 'method', 'statuscode'],
 });
