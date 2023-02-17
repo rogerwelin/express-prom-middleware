3ddd000563d6bc59035da9d3361d26049fc0a696
Roger Welin <roger.welin@icloud.com>
2023-02-17 10:49:18 +0100

    implement total requests and latency metrics

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import { collectDefaultMetrics, register } from 'prom-client';
-import { inProgress } from './metrics';
+import { inProgress, totalRequests, responseLatency } from './metrics';
 
 export interface PromOptions {
   metricsPath?: string;
@@ -16,13 +16,21 @@ export const promMiddleware = (options: PromOptions) => {
   }
 
   const promRedMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    let start: bigint;
+
     if (req.path !== options.metricsPath) {
+      start = process.hrtime.bigint();
       inProgress.inc({ path: req.path, method: req.method });
     }
 
     res.on('finish', () => {
       if (req.originalUrl !== options.metricsPath) {
-        inProgress.dec({ path: req.path, method: req.method });
+        inProgress.dec({ path: req.originalUrl, method: req.method });
+        totalRequests.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
+        const end = process.hrtime.bigint();
+        // Calculate the difference between the start time and end time in milliseconds
+        const diffInMs = Number(end - start) / 1000000;
+        responseLatency.observe({ path: req.originalUrl, method: req.method }, diffInMs);
       }
     });
 
--- src/metrics.ts
+++ src/metrics.ts
@@ -2,6 +2,18 @@ import { Counter, Gauge, Histogram, Summary } from 'prom-client';
 
 export const inProgress = new Gauge({
   name: 'http_requests_in_progress',
-  help: 'number of http requests in progress',
+  help: 'the count of HTTP requests that are currently in process',
+  labelNames: ['path', 'method'],
+});
+
+export const totalRequests = new Counter({
+  name: 'http_requests_total',
+  help: 'the total number of HTTP requests',
+  labelNames: ['path', 'method', 'statuscode'],
+});
+
+export const responseLatency = new Summary({
+  name: 'http_response_latency_ms',
+  help: 'the duration of responses in milliseconds',
   labelNames: ['path', 'method'],
 });
