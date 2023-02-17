10b55bd713c064db0c6d0fa6ada255b0ade84b98
Roger Welin <roger.welin@icloud.com>
2023-02-17 12:21:07 +0100

    comment codes intent and minor fixes

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export const promMiddleware = (options: PromOptions) => {
 
   const app: Express = express();
 
+  // initialize default metrics
   if (opts.collectDefaultMetrics) {
     collectDefaultMetrics();
   }
@@ -26,19 +27,26 @@ export const promMiddleware = (options: PromOptions) => {
   const promRedMiddleware = (req: Request, res: Response, next: NextFunction) => {
     let start: bigint;
 
-    if (req.path !== options.metricsPath) {
+    // increment ongoing requests count
+    if (req.path !== opts.metricsPath) {
       start = process.hrtime.bigint();
       ongoingRequests.inc({ path: req.path, method: req.method });
     }
 
+    // record metrics when response is finished
     res.on('finish', () => {
       if (req.originalUrl !== options.metricsPath) {
+        // decrement ongoing requests count
         ongoingRequests.dec({ path: req.originalUrl, method: req.method });
+
+        // record request count and response duration
         requestCount.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
         const end = process.hrtime.bigint();
         // Calculate the difference between the start time and end time in milliseconds
         const diffInMs = Number(end - start) / 1000000;
         responseDuration.observe({ path: req.originalUrl, method: req.method }, diffInMs);
+
+        // record error count and client error count
         if (res.statusCode >= 500 && res.statusCode <= 511) {
           errorCount.inc({ method: req.method, path: req.originalUrl, statuscode: res.statusCode });
         }
@@ -51,9 +59,11 @@ export const promMiddleware = (options: PromOptions) => {
     next();
   };
 
+  // use middleware function
   app.use(promRedMiddleware);
 
-  app.get((opts.metricsPath = '/metrics'), async (req: Request, res: Response) => {
+  // expose metrics endpoint
+  app.get(opts.metricsPath, async (req: Request, res: Response) => {
     res.set('Content-Type', register.contentType);
     res.end(await register.metrics());
   });
